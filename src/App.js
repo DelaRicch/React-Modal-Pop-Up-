4a8a8c65970b3072d7f7d32b883967a471bb1657
Dela Ricch <DelaRicch@gmail.com>
2022-08-21 15:52:13 +0000

    Done With Everythig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import Modal from "./components/Modal";
+import './index.css'
 
 function App() {
+
+  const [openModal, setOpenModal] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOpenModal(true)
+    }, 5000);
+  }, [])
+
   return (
     <div className="App">
-     <Modal />
+      <button className="modal-btn" onClick={() => setOpenModal(!openModal)}>Modal</button>
+      <Modal open={openModal} closeFunc={() => setOpenModal(false)} />
     </div>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './Modal.css'
+import ModalImg from './modalImg.jpg'
 
-const Modal = () => {
+const Modal = ({open, closeFunc}) => {
+
+    if (!open) return null
+
+    const Techs = [
+        {id: 1, name: "HTML"},
+        {id: 2, name: "CSS"},
+        {id: 3, name: "SCSS"},
+        {id: 4, name: "Tailwind CSS"},
+        {id: 5, name: "JavaScript"},
+        {id: 6, name: "React Js"},
+    ]
+
   return (
-    <section>Modal</section>
+    <section onClick={closeFunc} className='modal'>
+        <div onClick={(e) => {
+            e.stopPropagation()
+        }} className="container">
+            <img src={ModalImg} alt={ModalImg} />
+            <div className="right">
+                <div onClick={closeFunc} className="close-btn">X</div>
+                <div className="content">
+                    <div className="skills">
+                        <h1>Dela Ricch</h1>
+                        <h3>Front End Developer</h3>
+                        <div className="lang">
+                            {
+                                Techs.map(tech => (
+                                    <h5 key={tech.id}>{tech.name}</h5>
+                                ))
+                            }  
+                        </div>     
+                    </div>
+                </div>
+                <div className="buttons">
+                    <button className='contact'>Contact</button>
+                    <button className='resume'>Download Resume</button>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
